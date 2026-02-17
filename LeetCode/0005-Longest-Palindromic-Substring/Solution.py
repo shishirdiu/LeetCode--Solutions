8c564ea889bdd078379269f5c43d70db783697fa
Shishir Sarker <shishirsarker78@gmail.com>
2026-02-17 18:56:32 +0600

    Add longestPalindrome method to Solution class
    Implement a solution to find the longest palindromic substring in a given string.

--- LeetCode/0005-Longest-Palindromic-Substring/Solution.py
+++ LeetCode/0005-Longest-Palindromic-Substring/Solution.py
@@ -1,1 +1,24 @@
-
+class Solution:
+    def longestPalindrome(self, s):
+        if not s:
+            return ""
+        
+        res = ""
+        for i in range(len(s)):
+            
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > len(res):
+                    res = s[l : r + 1]
+                l -= 1
+                r += 1
+            
+            
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > len(res):
+                    res = s[l : r + 1]
+                l -= 1
+                r += 1
+                
+        return res
