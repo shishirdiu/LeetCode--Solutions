0dcb24237fb2be203b1f9ebfa868875634f93fa0
Shishir Sarker <shishirsarker78@gmail.com>
2026-02-17 19:09:28 +0600

    Implement reverse integer function in Solution.py

--- LeetCode/0007-Reverse-Integer/Solution.py
+++ LeetCode/0007-Reverse-Integer/Solution.py
@@ -1,1 +1,24 @@
-
+class Solution:
+    def reverse(self, x):
+        
+        MIN_INT = -2**31
+        MAX_INT = 2**31 - 1
+        
+        
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        
+        
+        res = 0
+        while x != 0:
+            digit = x % 10
+            res = res * 10 + digit
+            x //= 10
+        
+        res *= sign
+        
+        
+        if res < MIN_INT or res > MAX_INT:
+            return 0
+            
+        return res   
