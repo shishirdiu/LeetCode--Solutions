98021252b2ff23c137806ab01634fb9c39e10ab4
Shishir Sarker <shishirsarker78@gmail.com>
2026-02-17 19:02:42 +0600

    Implement Zigzag Conversion in Solution class

--- LeetCode/0006-Zigzag-Conversion/Solution.py
+++ LeetCode/0006-Zigzag-Conversion/Solution.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        rows = [''] * numRows
+        index = 0
+        step = 1
+        
+        for char in s:
+            rows[index] += char
+            
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+                
+            index += step
+            
+        return "".join(rows)
