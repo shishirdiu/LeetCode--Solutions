a7697c5b2db7609137ae478bd197b9f142b6b1c7
Shishir Sarker <shishirsarker78@gmail.com>
2026-02-18 12:27:46 +0600

    Implement myAtoi function for string to integer conversion

--- LeetCode/0008-String-to-Integer-atoi/Solution.py
+++ LeetCode/0008-String-to-Integer-atoi/Solution.py
@@ -1,1 +1,35 @@
-
+class Solution:
+    def myAtoi(self, s):
+        
+        s = s.strip()
+        if not s:
+            return 0
+        
+        
+        sign = 1
+        i = 0
+        
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+        
+        
+        res = 0
+        while i < len(s) and s[i].isdigit():
+            res = res * 10 + int(s[i])
+            i += 1
+        
+        
+        res = res * sign
+        
+        
+        INT_MIN, INT_MAX = -2147483648, 2147483647
+        
+        if res < INT_MIN:
+            return INT_MIN
+        if res > INT_MAX:
+            return INT_MAX
+            
+        return res
