8c528b6e8ca2b4f651f043327fe1ba4bb4dc950a
Shishir Sarker <shishirsarker78@gmail.com>
2026-03-03 13:54:42 +0600

    Add romanToInt method in Solution class
    Implement romanToInt method to convert Roman numerals to integers.

--- LeetCode/0013-Roman-to-Integer Solution.py
+++ LeetCode/0013-Roman-to-Integer Solution.py
@@ -1,1 +1,24 @@
-
+class Solution:
+    def romanToInt(self, s):
+        
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        res = 0
+        n = len(s)
+        
+        for i in range(n):
+            
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i+1]]:
+                res -= roman_map[s[i]]
+            else:
+                res += roman_map[s[i]]
+                
+        return res  
