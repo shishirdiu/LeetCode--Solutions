ef4c4dfddc62f1ffb0b08c10865c56f266045607
Shishir Sarker <shishirsarker78@gmail.com>
2026-03-03 13:58:12 +0600

    Add longestCommonPrefix method to Solution class
    Implement longestCommonPrefix method to find the longest common prefix among a list of strings.

--- LeetCode/0014-Longest-Common-Prefix Solution.py
+++ LeetCode/0014-Longest-Common-Prefix Solution.py
@@ -1,1 +1,20 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
 
+        
+        strs.sort()
+
+        
+        first = strs[0]
+        last = strs[-1]
+        res = ""
+
+      
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                return res
+            res += first[i]
+
+        return res
