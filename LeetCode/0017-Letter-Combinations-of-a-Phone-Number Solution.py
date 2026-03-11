6ca7b252073fc5fccac6ad77858909c63c649593
Shishir Sarker <shishirsarker78@gmail.com>
2026-03-11 11:13:28 +0600

    Implement letter combinations for phone number

--- LeetCode/0017-Letter-Combinations-of-a-Phone-Number Solution.py
+++ LeetCode/0017-Letter-Combinations-of-a-Phone-Number Solution.py
@@ -1,1 +1,27 @@
 
+class Solution:
+    def letterCombinations(self, digits):
+        if not digits:
+            return []
+        
+        
+        digit_to_char = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+        
+        res = []
+        
+        def backtrack(index, current_string):
+            
+            if len(current_string) == len(digits):
+                res.append(current_string)
+                return
+            
+           
+            for char in digit_to_char[digits[index]]:
+                backtrack(index + 1, current_string + char)
+        
+        
+        backtrack(0, "")
+        return res
