4d9d4cd6d29321c3e3a1534a5efd851f68ef392c
Shishir Sarker <shishirsarker78@gmail.com>
2026-03-11 11:17:13 +0600

    Add fourSum method to Solution class
    Implement fourSum function to find quadruplets that sum to target.

--- LeetCode/0018-4Sum Solution.py
+++ LeetCode/0018-4Sum Solution.py
@@ -1,1 +1,31 @@
+class Solution:
+    def fourSum(self, nums, target):
+        nums.sort()
+        res = []
+        n = len(nums)
 
+        for i in range(n):
+            
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            for j in range(i + 1, n):
+               
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+               
+                l, r = j + 1, n - 1
+                while l < r:
+                    curr_sum = nums[i] + nums[j] + nums[l] + nums[r]
+                    
+                    if curr_sum == target:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        l += 1
+                        
+                        while l < r and nums[l] == nums[l - 1]:
+                            l += 1
+                    elif curr_sum < target:
+                        l += 1
+                    else:
+                        r -= 1
+        return res
