f3120a11964d41b18fcf8a96332978d4f733781f
Shishir Sarker <shishirsarker78@gmail.com>
2026-03-11 11:21:02 +0600

    Add removeNthFromEnd method to Solution class
    Implement a method to remove the nth node from the end of a linked list using a two-pointer approach.

--- LeetCode/0019-Remove-Nth-Node-From-End-of-List Solution.py
+++ LeetCode/0019-Remove-Nth-Node-From-End-of-List Solution.py
@@ -1,1 +1,20 @@
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
 
+        
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+        
+        
+        while right:
+            left = left.next
+            right = right.next
+        
+       
+        left.next = left.next.next
+        
+        return dummy.next
